6013b2856787c3806d0889df127a350565133a15
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-03-31 18:32:49 +0000

    commit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -24,36 +24,39 @@ const Login = ({ darkModeValue }) => {
     }
 
     return (
-        <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-            <div data-theme={darkModeValue} className="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
-                <div className="hero-body">
-                    <div className="container">
-                        <div className="columns is-centered">
-                            <div className="column is-4-desktop">
-                                <form onSubmit={Auth} className="box">
-                                    <p className="has-text-centered">{msg}</p>
-                                    <div className="field mt-5">
-                                        <label className="label">Email or Username</label>
-                                        <div className="controls">
-                                            <input type="text" className="input" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)} />
-                                        </div>
-                                    </div>
-                                    <div className="field mt-5">
-                                        <label className="label">Password</label>
-                                        <div className="controls">
-                                            <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
-                                        </div>
-                                    </div>
-                                    <div className="field mt-5">
-                                        <button className="btn btn-outline btn-secondary">Login</button>
-                                    </div>
-                                </form>
+            <div data-theme={darkModeValue} className="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20" style={{ height: "93vh" }}>
+                <div className="hero ">
+                    <div className="hero-content flex-col lg:flex-row-reverse">
+                        <div className="text-center lg:text-left">
+                            <h1 className="text-5xl font-bold">Login now!</h1>
+                            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        </div>
+                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                            <div className="card-body">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="text" className="input" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                    <label className="label">
+                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    </label>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Login</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+
     )
 }
 
