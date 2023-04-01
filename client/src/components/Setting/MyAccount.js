398354c02fafcb564c97c45cf2b566a6dee331c6
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-04-01 19:53:28 +0000

    commit

---
+++ client/src/components/Setting/MyAccount.js
@@ -0,0 +1,16 @@
+
+const MyAccount = () => {
+
+    return (
+        <div className="mockup-window border border-base-300">
+            <center className="my-4 text-4xl font-bold text-gray-700 dark:text-gray-100">My account</center>
+            <div className="flex justify-center px-4 py-16 bg-base-200">
+                <input type="text" placeholder="Type here" className="input input-bordered input-success w-full max-w-xs" />
+
+            </div>
+        </div>
+    )
+}
+
+
+export default MyAccount; 
