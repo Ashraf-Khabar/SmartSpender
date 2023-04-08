fd841a5389db569a334bb0509369134378abb7b6
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-04-08 20:30:54 +0000

    commit

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Divisions from './dashboard/Divisions'
 import Expenses from './dashboard/Expenses';
+import DivisionsDash from './DivisionsDash';
 
 
 const Dashboard = ({ darkModeValue }) => {
     return (
         <div data-theme={darkModeValue} className="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
-            {<Divisions darkModeValue={darkModeValue} />}
+            {<DivisionsDash darkModeValue={darkModeValue} />}
             <br/><br/>
             {<Expenses darkModeValue={darkModeValue} />}
         </div>
